d87a9bcbb71bd5071dd54c86ce995ffb123034a9
JUSTIN PEREZ <u1248103@umail.utah.edu>
2022-09-27 11:25:23 -0600

    making a change to see if it works

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jake is honestly the gayest man alive.
         </p>
         <a
           className="App-link"
